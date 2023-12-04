139a2cfd91fe0e0407d63a9acf9c5d758ef987ff
darrenoml <144786470+darrenoml@users.noreply.github.com>
2023-12-04 15:58:07 +0700

    Update ExerciseDriverCDM.py

--- ExerciseDriverCDM.py
+++ ExerciseDriverCDM.py
@@ -1,27 +1,41 @@
-import ProgramExerciseCDM
-def ItemList():
-    Items = []
-    NumItems = int(input("Enter the number of items you wish to add"))
-    while True:
-        try:
-            if NumItems < 1:
-                print("Please enter a number bigger than 1")
-                break
-        except ValueError:
-            print("Invalid Number")
+from ProgramExerciseCDM import Inventory
 
-    for i in range(NumItems):
-        print(f"Item (i+1)")
-        while True:
-            food = input("Enter food:")
-            amount = float(input("Enter amount:"))
-            if amount <= 0:
-                print("amount must be greater than 0")
-                break
-        Items.append(ProgramExerciseCDM(food, amount))
-    return Items
-
-def display(Items):
-    for item in list:
-        print(f"Item:")
+def create_objects_list():
+    items_list = []
+    num_items = int(input("How many items will you order today? "))
+    while num_items < 1:
+        print("Number of items must be at least 1.")
+        num_items = int(input("How many items will you order today? "))
+    
+    for i in range(num_items):
+        food = input(f"Item #{i + 1}-\nEnter food: ")
+        amount = float(input("Enter amount of pounds: "))
+        while amount <= 0:
+            print("Amount of pounds must be greater than 0.")
+            amount = float(input("Enter amount of pounds: "))
         
+        item = Inventory(food, amount)
+        items_list.append(item)
+    
+    return items_list
+
+def display_items_list(items_list):
+    for item in items_list:
+        print("\nItem:", item.get_food())
+        print("Amount ordered:", f"{item.get_amount()}", "pounds")
+        print("Price per pound: $", f"{item.PriceList()}")
+        print("Price of order: $", f"{item.calculate()}")
+
+def calculate_total_cost(items_list):
+    total_cost = 0
+    for item in items_list:
+        total_cost += item.get_calculated_price()
+    return total_cost
+
+def main():
+    items_list = create_objects_list()
+    display_items_list(items_list)
+    total_cost = calculate_total_cost(items_list)
+    print("Total Cost: $", (total_cost))
+
+main()
